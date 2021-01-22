223e01ce984827de2346e8817d97eeaa226c225b
acer <wonverin@gmail.com>
2021-01-22 09:04:49 +0100

    start

---
+++ cmake-build-debug/CMakeFiles/NaGita.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NaGita.dir/main.c.obj"
+  "NaGita.exe"
+  "NaGita.exe.manifest"
+  "NaGita.pdb"
+  "libNaGita.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NaGita.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NaGita.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NaGita.
+# This may be replaced when dependencies are built.
